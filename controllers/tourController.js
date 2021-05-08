7488cd3d17c5dcb14843c027cda67e6784ff04ea
Sairoden <odingandarosa@gmail.com>
2021-05-08 22:14:02 +0800

    add field limit and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,16 +4,46 @@ exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
     console.log(req.query);
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+      console.log(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3) Field limting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
