6b77dc3317ad5b2b09b2ab2983db20605271e078
Sairoden <odingandarosa@gmail.com>
2021-05-09 15:15:03 +0800

    add monthlyPlan routes and function

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,11 +5,16 @@ const router = express.Router();
 
 // router.param("id", tourController.checkID);
 
+// Top 5 cheap tours
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+// Tour Stats
 router.route("/tour-stats").get(tourController.getTourStats);
+
+// Tour monthly plan
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 
 // Tours
 router
