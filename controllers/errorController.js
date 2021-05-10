4f7a7c6ad362edca9faed7dcac31b219bc2c968c
Sairoden <odingandarosa@gmail.com>
2021-05-10 19:39:50 +0800

    add function handler of CastError,DuplicateFields,ValidationErrors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: "${value}". Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -9,11 +29,12 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
-  if (err.isOperational)
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+  }
   // Programming or other unknown error: don't leak error details
   else {
     // 1) Log error
@@ -27,10 +48,21 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+console.log(process.env.NODE_ENV);
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") sendErrorDev(err, res);
-  else if (process.env.NODE_ENV === "production") sendErrorProd(err, res);
+  else if (process.env.NODE_ENV === "production") {
+    let error = err;
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
