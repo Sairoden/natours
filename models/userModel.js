75ba93313ded9032dd25195307ec7e0d55e03cd5
Sairoden <odingandarosa@gmail.com>
2021-05-11 11:37:40 +0800

    encrpyt password using bcrpyt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -25,7 +26,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: true,
+    validate: {
+      // This only works on CREATE / SAVE!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same!",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
