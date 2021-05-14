ff0c4bedf6210d8382b0b089702221b112228027
Sairoden <odingandarosa@gmail.com>
2021-05-14 14:44:26 +0800

    Improve read performance with indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -119,6 +119,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // Virtual properties
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
