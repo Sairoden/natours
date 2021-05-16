12533cd66cb6fe012ed09044e042119df0b9c95e
Sairoden <odingandarosa@gmail.com>
2021-05-16 12:41:17 +0800

    Build Tour Page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,9 +6,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
 
   // 2) Build template
-
   // 3) Render that templates using tour data from step 1
-
   res.status(200).render("overview", {
     title: "All Tours",
     tours,
@@ -16,7 +14,16 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user",
+  });
+
+  // 2) Build templates
+  // 3) Render template using step 1
   res.status(200).render("tour", {
     title: "The Forest Hiker",
+    tour,
   });
 });
